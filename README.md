bf4ce14e269d248fe5d02cf40c24cf9d83b4bb03
dokechin <user@user-PC.(none)>
2014-02-04 22:16:06 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Hakomonos - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Hakomonos;
+
+# DESCRIPTION
+
+Acme::Hakomonos is ...
+
+# LICENSE
+
+Copyright (C) dokechin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+dokechin <>
---
+++ lib/Acme/Hakomonos.pm
@@ -0,0 +1,39 @@
+package Acme::Hakomonos;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Hakomonos - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Hakomonos;
+
+=head1 DESCRIPTION
+
+Acme::Hakomonos is ...
+
+=head1 LICENSE
+
+Copyright (C) dokechin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+dokechin E<lt>E<gt>
+
+=cut
+
